18ee2a983f761c24478918b61c2d28f8124fb8af
julia <julia.zhyvylo@asila.mk>
2024-10-11 20:11:26 +0200

    task 1 done

---
+++ js/task-1.js
@@ -0,0 +1,13 @@
+'use strict';
+
+function makeTransaction(quantity, pricePerDroid, customerCredits) {
+  const totalCost = quantity * pricePerDroid;
+  if (totalCost > customerCredits) return 'Insufficient funds!';
+  else return `Your ordered ${quantity} droids worth ${totalCost} credits!`;
+}
+
+console.log(makeTransaction(5, 3000, 23000)); // "You ordered 5 droids worth 15000 credits!"
+console.log(makeTransaction(3, 1000, 15000)); // "You ordered 3 droids worth 3000 credits!"
+console.log(makeTransaction(10, 5000, 8000)); // "Insufficient funds!"
+console.log(makeTransaction(8, 2000, 10000)); // "Insufficient funds!"
+console.log(makeTransaction(10, 500, 5000)); // "You ordered 10 droids worth 5000 credits!"
